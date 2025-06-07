3e888dccc418f243ffcb04ba4b898bd1363eb19b
anuragparashar26 <anuragparashar5025@gmail.com>
2025-06-07 21:04:22 +0530

    implemented backend

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      }
+    }
   },
   server: {
     port: process.env.PORT || 5173,
