bf829092dbd30cd260cd7fc66b911cdfa86b9462
anuragparashar26 <anuragparashar5025@gmail.com>
2025-06-07 12:51:32 +0530

    Allow Render host in Vite preview

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    define: {
-        'process.env': process.env
-    }
+  plugins: [react()],
+  define: {
+    'process.env': process.env
+  },
+  preview: {
+    allowedHosts: ['cybertron-p1tc.onrender.com'] 
+  }
 });
